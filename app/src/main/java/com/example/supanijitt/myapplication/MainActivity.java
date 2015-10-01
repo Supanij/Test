987b259651f5dffcaf76be55209d087a1b9c20d1
Supanij <supanij.ittiteerarak@gmail.com>
2015-10-01 09:45:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/supanijitt/myapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.supanijitt.myapplication;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText txt_user, txt_pass;
+    TextView txt_regist;
+    Button btn_Login, btn_LoginFb, btn_LoginGp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_user = (EditText)findViewById(R.id.userName);
+        txt_pass = (EditText)findViewById(R.id.passWord);
+        txt_regist = (TextView)findViewById(R.id.register);
+        btn_Login = (Button)findViewById(R.id.btnLogin);
+        btn_LoginFb = (Button)findViewById(R.id.btnFb);
+        btn_LoginGp = (Button)findViewById(R.id.btnGp);
+
+        btn_Login.setOnClickListener(this);
+        btn_LoginFb.setOnClickListener(this);
+        btn_LoginGp.setOnClickListener(this);
+        txt_regist.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String sUsername = txt_user.getText().toString();
+        String sPassword = txt_pass.getText().toString();
+
+        switch (v.getId()){
+            case R.id.btnLogin :
+                Login(sUsername);//Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnFb :
+                Toast.makeText(getApplicationContext(), sUsername , Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnGp :
+                Toast.makeText(getApplicationContext(), sUsername.toString() , Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.register :
+                //Toast.makeText(getApplicationContext(), "Register !!!", Toast.LENGTH_LONG).show();
+                msgBox("Register..");
+                break;
+            default: break;
+        }
+    }
+
+    public void msgBox(String s){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setTitle("Fisrt Dialog");
+        alertDialog.setMessage(s);
+        alertDialog.show();
+    }
+
+    public void Login(String s){
+        //Log.d("s : ", s.toString());
+        Intent i = new Intent(this, NextPageActivity.class);
+
+        i.putExtra("name",s);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/supanijitt/myapplication/NextPageActivity.java
@@ -0,0 +1,57 @@
+package com.example.supanijitt.myapplication;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NextPageActivity extends AppCompatActivity {
+    TextView dispName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_page);
+
+        dispName = (TextView)findViewById(R.id.displayName);
+
+        Intent _b = getIntent();
+        String str = _b.getStringExtra("name");
+        Log.d("str : ",str.toString());
+        dispName.setText(str);
+        /*if(_b.){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which){ finish();}
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.setTitle("Error Dialog");
+            alertDialog.setMessage("No display name...");
+
+            alertDialog.show();
+        }
+        else {
+            String str = _b.getString("name");
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which){ finish();}
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.setTitle("Success Dialog");
+            alertDialog.setMessage(str);
+
+            alertDialog.show();
+
+            dispName.setText(str);
+        }*/
+    }
+
+
+}
